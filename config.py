897b78706d59b86ed29a13ecc36217f510be951e
yassuhacks750 <yassudj8085231272@gmail.com>
2025-05-29 16:23:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "22849789"
+api_hash = "0fc127c6055acd59f00ec6c229e1e3c4"
+bot_token = "8171819457:AAEQPzj19DDqD6Xd0Xm7nTTZvgLgE-tjWww"
+auth_users = [1144684909]
